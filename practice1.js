5e43601da2534d8bb593cc68327c814d066060af
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-05-01 14:19:51 -0700

    Update practice1.js

--- practice1.js
+++ practice1.js
@@ -4,9 +4,23 @@ function App() {
     const name = React
   return (
     <div>
-        {name === React ? }
+        {name === React ? <h1>I'm React</h1> : <h2>I'm not React</h2>}
     </div>
   );
+}
+
+function AppII() {
+  const name = React
+return (
+  <div>
+      {name === React &&  <h1>I'm React</h1>}
+  </div>
+);
+}
+
+function AppIII() {
+  const name = undefined
+return name || 'this is undefined';
 }
 
 export default App;
