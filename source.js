a35aecc677e6ee707578281ac506ef6ce4c08b2b
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-05-07 11:21:12 -0700

    Create source.js

---
+++ source.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import root from 'root';
+
+/* 
+fuction App() {
+    render() {
+        return(
+            <div>
+                <h1>Hellow React!</h1>
+            </div>
+        ), document.getElementById('root');
+    }
+}
+*/
+
+ReactDOM.render(
+    <h1>Hellow React!</h1>, document.getElementById('root')
+);
+
